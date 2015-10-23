16cf85c02ea6f1b689c271fb2529718aef9e1c3d
Fernando Santos <fus@certi.org.br>
2015-10-23 16:47:31 -0200

    MakeCacheMatrix
    Duas funcoes que retornarao do cache matriz invertida
    
    Function 1  makeCacheMatrix cria uma matriz especial a ser invertida
    Function 2  cacheSolve Calcula a o inverso da Matriz obtida no
    MakeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+library(da
+        ## Duas funcoes que retornarao do cache matriz invertida
+        ## makeCacheMatrix cria uma matriz especial a ser invertida
+        
+        ##
+        
+        makeCacheMatrix <- function( x = matrix() ) {
+                
+                ## seta a propriedade de inversao
+                
+                a <- NULL
+                
+                ## Definicao da Matriz
+                
+                set <- function( matrix ) {
+                        g <<- matrix
+                        a <<- NULL
+                }
+                
+                ## Obtencao da Matriz
+                
+                get <- function() {
+                        
+                        g
+                }
+                
+                ## Metodo para inversao
+                
+                SetMatrizInversa <- function(inverse) {
+                        
+                        a <<- inverse
+                }
+                
+                ## Obtencao da matriz inversa
+                getMatrizInversa <- function() {
+                        
+                        a
+                }
+                
+                ## lista os metodos
+                
+                list(set = set, get = get,
+                     SetMatrizInversa = SetMatrizInversa,
+                     getMatrizInversa = getMatrizInversa)
+        }
+        ## Calcula a o inverso da Matriz obtida no MakeCacheMatrix 
+        ## Caso o inverso ja tenha sido calculado a e a matriz nao tenha se alterado esta funcao devera retornar o inverso do cache
+        
+        cacheSolve <- function(x, ...) {
+                
+                
+                g <- x$getMatrizInversa()
+                
+                ## Houve mudanca?
+                
+                if( !is.null(g) ) {
+                        message("Obtendo o dado do Cache")
+                        return(g)
+                }
+                
+                ## Busca a matriz 
+                
+                data <- x$get()
+                
+                ## calcula o inverso
+                
+                g <- solve(data) %*% data
+                
+                ## estabele o inverso do objeto g
+                
+                x$SetMatrizInversa(g)
+                
+                ## retorna a matriz
+                
+                g
+        } 
+        
